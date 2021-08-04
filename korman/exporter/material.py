ca09048abfe06caa70b097b5a119ec588ed350e9
Patrick Dulebohn <Ptrck10381@gmail.com>
2021-08-04 18:36:01 -0400

    Remove changes from another branch.

--- korman/exporter/material.py
+++ korman/exporter/material.py
@@ -480,8 +480,6 @@ class MaterialConverter:
             layer.opacity = layer_props.opacity / 100
             if layer_props.opacity < 100 and not state.blendFlags & hsGMatState.kBlendMask:
                 state.blendFlags |= hsGMatState.kBlendAlpha
-            if layer_props.use_alpha_vcol:
-                state.blendFlags |= hsGMatState.kBlendAlpha
             if layer_props.alpha_halo:
                 state.blendFlags |= hsGMatState.kBlendAlphaTestHigh
             if layer_props.z_bias:
